534fc46f7592ca4b41409edde71731a19649c4aa
Vigneron <vincent.vigneron@univ-angers.fr>
2016-04-18 19:00:21 +0200

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -29,27 +29,3 @@ fn main() {
         _ => process::exit(1),
     }
 }
-
-/*
-fn main() {
-    // (ab)*c
-    let file = "data/dfa1.txt";
-    match DFA::new_from_file(file) {
-        Ok(d) => {
-            let dfa = d;
-            println!("{}", dfa);
-            println!("{:?}", dfa.run("ababac"));
-            println!("{:?}", dfa.run("ababc"));
-            println!("{:?}", dfa.run(""));
-            println!("{:?}", dfa.run("abc"));
-            println!("{:?}", dfa.run("ac"));
-            println!("{:?}", dfa.run("c"));
-            println!("{:?}", dfa.run("ababababababababababababababababababababc"));
-        },
-        Err(e) => {
-            println!("{}", e);
-            //process::exit(0)
-        },
-    }
-}
-*/
